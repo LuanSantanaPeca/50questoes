974420390d6c1e233f8567c77af9db1ec3c796ea
Pehssa <154289664+LuanSantanaPeca@users.noreply.github.com>
2025-09-11 12:41:14 -0300

    Add files via upload

---
+++ atv24.php
@@ -0,0 +1,17 @@
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25164713-2 - Luan Enrico Santana Peça
+ 25161143-2 - Gabriel Hikari Uchida Requião 
+ 25356144-2 - Lucas Teixeira da Silva
+ 25001132-2 - Cauã Chaerki Bobato
+ 25068539-2 - Guilherme Costa Ferreira
+ 25324764-2 - Gabriel Eduardo Diniz
+ 25016187-2 - Michel Bueno Chacon
+ 25001459-2 - Eduardo Gritten dos Santos Spohr
+Data:
+Descritivo:
+*******************************************************************************/
