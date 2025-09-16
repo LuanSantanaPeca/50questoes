e7c80aa6324b644d05603b1187c920e50ea7a17e
Pehssa <154289664+LuanSantanaPeca@users.noreply.github.com>
2025-09-16 16:27:14 -0300

    Update atv37.php

--- atv37.php
+++ atv37.php
@@ -23,8 +23,8 @@ $a = 3;
 $b = 4;
 $c = 5;
 if ($a + $b > $c && $a + $c > $b && $b + $c > $a) {
-    echo "Triângulo válido<br>";
+    echo "Triângulo válido\n";
 } else {
-    echo "Triângulo inválido<br>";
+    echo "Triângulo inválido\n";
 }
 ?>
