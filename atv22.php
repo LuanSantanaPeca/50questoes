3aea04a5c9c0e4e14e3743519a4408eefff4d0dd
Pehssa <154289664+LuanSantanaPeca@users.noreply.github.com>
2025-09-16 15:36:44 -0300

    Update atv22.php

--- atv22.php
+++ atv22.php
@@ -14,7 +14,7 @@ Componentes:
  25001459-2 - Eduardo Gritten dos Santos Spohr
  25142481-2 - 
  25356591-2 - Sadrak Araújo Leopoldino
-Data: 15 de setembro de 2025
+Data: 15/09/2025
 Descritivo: Compare se null é igual a false
 *******************************************************************************/
 <?php
