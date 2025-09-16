ac68a85b482a80ae201852c3f9b9211a0e6c2a18
Pehssa <154289664+LuanSantanaPeca@users.noreply.github.com>
2025-09-16 16:35:18 -0300

    Update atv50.php

--- atv50.php
+++ atv50.php
@@ -12,7 +12,7 @@ Componentes:
  25324764-2 - Gabriel Eduardo Diniz
  25016187-2 - Michel Bueno Chacon
  25001459-2 - Eduardo Gritten dos Santos Spohr
- 25142481-2 - 
+ 25142481-2 - Isabelli Azevedo da Silva
  25356591-2 - Sadrak Araújo Leopoldino
 Data: 10 de Setembro de 2025
 Descritivo: Crie uma expressão ternária que verifique se um ano é bissexto
