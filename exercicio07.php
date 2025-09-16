35bfe94148ccb638adfa8aa485daa9cd702bc517
Pehssa <154289664+LuanSantanaPeca@users.noreply.github.com>
2025-09-16 14:09:03 -0300

    Update exercicio07.php

--- exercicio07.php
+++ exercicio07.php
@@ -12,6 +12,8 @@ Componentes:
  25324764-2 - Gabriel Eduardo Diniz
  25016187-2 - Michel Bueno Chacon
  25001459-2 - Eduardo Gritten dos Santos Spohr
+ 25142481-2 - 
+ 25356591-2 - Sadrak Araújo Leopoldino
 Data: 15 de Setembro de 2025
 Descritivo: Escreva um programa que converta graus Celsius para Fahrenheit (F = C × 9/5 + 32)
 *******************************************************************************/
@@ -27,3 +29,4 @@ echo "<h3>Conversão de Celsius para Fahrenheit</h3>";
 echo "$celsius °C = $fahrenheit °F";
 
 ?>
+
